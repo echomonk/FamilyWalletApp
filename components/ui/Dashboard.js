918b59530949f39de899c3a26b51c1e103260015
echomonk <erdaloylupinar@gmail.com>
2022-04-01 15:25:18 +0300

    Create Dashboard.js

---
+++ components/ui/Dashboard.js
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+    return (
+      <div className="flex items-start justify-center">
+         <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 dashboardCard .blue-glassmorphism">
+
+        </div>
+      </div>
+      
+    );
+  }
+  
+  export default Dashboard;
