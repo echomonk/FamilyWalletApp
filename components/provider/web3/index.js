e1d796927672d8e7d686850579eee68584f4325e
echomonk <erdaloylupinar@gmail.com>
2022-04-01 19:40:26 +0300

    Update index.js

--- components/provider/web3/index.js
+++ components/provider/web3/index.js
@@ -1,4 +1,4 @@
-const { createContext, useContext, useEffect, useState } = require("react");
+const { createContext, useContext, useEffect, useState, useMemo } = require("react");
 
 import detectEthereumProvider from "@metamask/detect-provider";
 import Web3 from "web3";
@@ -33,6 +33,21 @@ export default function Web3Provider({children}) {
 
     loadProvider()
   }, [])
+
+  const _web3Api = useMemo(() => {
+    return {
+      ...web3Api,
+      connect: web3Api.provider ?
+        async () => {
+          try {
+            await web3Api.provider.request({method: "eth_requestAccounts"})
+          } catch {
+            location.reload()
+          }
+        } :
+        () => console.error("Cannot connect to Metamask, try to reload your browser please.")
+    }
+  }, [web3Api])
 
   return (
     <Web3Context.Provider value={web3Api}>
