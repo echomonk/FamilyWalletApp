c0fb48d24c2a9ed6b3f0eb6f108025306d49242d
echomonk <erdaloylupinar@gmail.com>
2022-04-01 20:30:35 +0300

    Create index.js

---
+++ components/ui/button/index.js
@@ -0,0 +1,18 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+
+
+export default function Button({ 
+    children,
+    className = "bg-blue-500 hover:bg-blue-600 text-white text-base font-semibold",
+    ...rest
+}) {
+ 
+  return (
+    <span
+        {...rest}
+        className={`flex flex-grow w-full justify-center items-center my-5 p-3 rounded-full cursor-pointer ${className}`}>
+        <AiFillPlayCircle className="text-white mr-2"/>
+         {children}
+    </span>
+  )
+}
