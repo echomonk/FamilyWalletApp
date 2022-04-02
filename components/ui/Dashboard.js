e5d332726e2d4a8d207992fb1fc268b137ae6a4a
echomonk <erdaloylupinar@gmail.com>
2022-04-02 20:27:31 +0300

    Update Dashboard.js

--- components/ui/Dashboard.js
+++ components/ui/Dashboard.js
@@ -1,6 +1,6 @@
 const Dashboard = ({network}) => {
     return (
-      <div className="flex flex-grow w-full justify-center items-center my-5 p-3 rounded-full blue-glassmorphism">
+      <div className="flex flex-grow w-full justify-center items-center text-white my-5 p-3 rounded-full blue-glassmorphism">
         {network} 
       </div>
       
