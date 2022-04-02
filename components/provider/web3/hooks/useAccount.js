812d83bcb6fac9a770196093859b90fc0f80d4ae
echomonk <erdaloylupinar@gmail.com>
2022-04-02 20:22:09 +0300

    Update useAccount.js

--- components/provider/web3/hooks/useAccount.js
+++ components/provider/web3/hooks/useAccount.js
@@ -1,9 +1,13 @@
 import { useEffect } from "react"
 import useSWR from "swr"
 
+const adminAddresses = {
+ "0x79a4bf16935602e50c7ec1f30f5b133e9749bc7c4cc735c0ba842e042956503b": true
+}
+
 export const handler = (web3, provider) => () => {
 
-  const { mutate, ...rest } = useSWR(() =>
+  const { data, mutate, ...rest } = useSWR(() =>
     web3 ? "web3/accounts" : null,
     async () => {
       const accounts = await web3.eth.getAccounts()
@@ -18,5 +22,13 @@ export const handler = (web3, provider) => () => {
     )
   }, [provider])
 
-  return { account: {mutate, ...rest }}
+  return { 
+    account: {
+      data,
+      isAdmin: (
+        data && 
+        adminAddresses[web3.utils.keccak256(data)]) ?? false,
+      mutate,
+       ...rest
+   }}
 }
