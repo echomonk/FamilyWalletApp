b3f8f1f4b4bebd3db5164919f69363a4e9e46b29
echomonk <erdaloylupinar@gmail.com>
2022-04-03 13:16:25 +0300

    Update index.js

--- components/provider/web3/index.js
+++ components/provider/web3/index.js
@@ -12,7 +12,8 @@ export default function Web3Provider({children}) {
     provider: null,
     web3: null,
     contract: null,
-    isLoading: true
+    isLoading: true,
+    hooks: setupHooks()
   })
 
   useEffect(() => {
@@ -27,7 +28,8 @@ export default function Web3Provider({children}) {
           provider,
           web3,
           contract: null,
-          isLoading: false
+          isLoading: false,
+          hooks: setupHooks(web3, provider)
         })
       } else {
         setWeb3Api(api => ({...api, isLoading: false}))
@@ -43,7 +45,6 @@ export default function Web3Provider({children}) {
     return {
       ...web3Api,
       isWeb3Loaded: web3 != null,
-      getHooks: () => setupHooks(web3, provider),
       connect: provider ?
         async () => {
           try {
@@ -68,6 +69,6 @@ export function useWeb3() {
 }
 
 export function useHooks(cb) {
-  const { getHooks } = useWeb3()
-  return cb(getHooks())
+  const { hooks } = useWeb3()
+  return cb(hooks)
 }
