1c2631ecab0268d932d4970fb52c6ed16ea2d9ae
echomonk <erdaloylupinar@gmail.com>
2022-04-03 13:55:50 +0300

    Update useSetupHooks.js

--- components/provider/web3/hooks/useSetupHooks.js
+++ components/provider/web3/hooks/useSetupHooks.js
@@ -4,7 +4,6 @@ import { handler as createAccountHook } from "./useAccount";
 import { handler as createNetworkHook } from "./useNetwork";
 
 export const setupHooks = (...deps) => {
-  console.log("setting up hooks")
   return {
     useAccount: createAccountHook(...deps),
     useNetwork: createNetworkHook(...deps)
