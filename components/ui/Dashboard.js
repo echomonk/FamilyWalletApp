9c7ec53eec570cda60373117dc594e76b2ffa657
echomonk <erdaloylupinar@gmail.com>
2022-04-03 13:16:27 +0300

    Update Dashboard.js

--- components/ui/Dashboard.js
+++ components/ui/Dashboard.js
@@ -1,10 +1,62 @@
-const Dashboard = ({network}) => {
+import { useAccount, useNetwork } from "@components/web3/hooks";
+
+
+const Dashboard = ({}) => {
+
+  const { account } = useAccount()
+  const { network } = useNetwork()
+
+
     return (
-      <div className="flex flex-grow w-full justify-center items-center text-white my-5 p-3 rounded-full blue-glassmorphism">
-        {network} 
+      <div className="flex flex-col w-full justify-center items-center my-5 p-3 blue-glassmorphism">
+          {/* { account.isAdmin ?
+            <span
+              className="flex flex-grow w-full justify-center items-center my-5 p-3 rounded-full bg-orange-500 hover:bg-orange-600 text-white text-base font-semibold">
+              Welcome Admin
+            </span> :
+            <>
+            </>
+          }  */}
+
+          <span className="flex flex-col w-full justify-center items-center my-5 p-1 text-base font-semibold white-glassmorphism">
+          { account.isAdmin ?
+            <span
+              className="flex flex-grow w-full justify-center items-center my-0.5 p-2 rounded-full bg-orange-400 hover:bg-orange-500 text-white text-base font-semibold">
+              Welcome Admin
+            </span> :
+            <>
+            </>
+          } 
+          { network.hasInitialResponse && !network.isSupported &&
+             <div className="my-0.5 w-full rounded-full p-2 outline-none bg-transparent text-red-500 border-none text-sm white-glassmorphism">
+               Wrong network! Connect to:  {network.target}
+               </div>
+           }
+            { network.data &&
+             <div className="my-0.5 w-full rounded-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism">
+                Currently on: {network.data}
+               </div>
+           }
+
+          </span>
+        <div className="my-2 w-full rounded-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism">
+           { network.data }
+        </div>
+        <div className="my-2 w-full rounded-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism">
+           { network.data }
+        </div>
+        <div className="my-2 w-full rounded-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism">
+           { network.data }
+        </div>
+        <div className="my-2 w-full rounded-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism">
+           { network.data }
+        </div>
+        
       </div>
       
     );
   }
   
   export default Dashboard;
+
+ // p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism
