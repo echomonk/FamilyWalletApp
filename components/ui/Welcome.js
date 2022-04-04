aa7fcbc69b87c3e31204375b2af7fe27c779676a
echomonk <erdaloylupinar@gmail.com>
2022-04-04 23:40:50 +0300

    Update Welcome.js

--- components/ui/Welcome.js
+++ components/ui/Welcome.js
@@ -1,4 +1,5 @@
 import Button from "./button";
+import Dashboard from "./Dashboard";
 
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
@@ -6,32 +7,19 @@ import { Loader } from "./";
 import { shortenAddress } from "@components/provider/web3/hooks/shortenAddress";
 import { useEthPrice } from "@components/provider/web3/hooks/useEthPrice";
 import { useWeb3 } from "@components/provider";
-import Dashboard from "./Dashboard";
 import { useAccount, useNetwork } from "@components/web3/hooks";
+import { SetForm } from "@components/context";
+
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({ placeholder, name, type, value, handleChange }) => (
-  <input 
-    placeholder={placeholder}
-    type={type}
-    min="0.0000"
-    step="0.0001"
-    value={value}
-    onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-  />
-)
 
 export default function Welcome() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
   const { account } = useAccount()
   const { network } = useNetwork()
   const { eth } = useEthPrice()
-
-  const handleSubmit = () => {
-
-  }
+  
      return (
       <div className="flex w-full justify-center items-start">
         <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -40,17 +28,12 @@ export default function Welcome() {
               <h1 className="text-3xl text-center sm:text-5xl text-white text-gradient py-1">
                 A family wallet app <br /> with independent transfer functionality.
               </h1>
-              {/* <>
-              "Current" {`${network.data}`}
-              "Target" {`${network.target}`}
-              "Is Supported" {`${network.isSupported}`}
-              </> */}
               <p className="text-center mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                 Explore the crypto world. Buy and sell cryptocurrencies easily.
               </p>
             </div>
               
-            { !account.data ? 
+            { !account.data ?
               (isLoading ? 
                   <Button
                     onClick={connect}>
@@ -68,9 +51,10 @@ export default function Welcome() {
                 ) :
                 <></>
              }
-  
-               <Dashboard network={network.data} />
-
+              { account.data && account.hasInitialResponse ?
+                <Dashboard network={network.data} /> :
+               <></>
+              }
              
               <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                 <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -127,29 +111,8 @@ export default function Welcome() {
                 </div>
               </div>
             </div>
-
-
-
-            <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-              <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} /> 
-              <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
-              <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-              <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} /> 
-
-              <div className="h-[1px] w-full bg-gray-400 my-2" />
-
-              { false ? (
-                <Loader />
-              ) : (
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                  Send Now
-                </button>
-              )}
-            </div>
+            
+            <SetForm />
 
           </div>
         </div>
