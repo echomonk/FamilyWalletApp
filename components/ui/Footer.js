7cdfa134daa505bb9808433b637d57902dff8d6a
echomonk <erdaloylupinar@gmail.com>
2022-04-07 19:40:35 +0300

    Update Footer.js

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -1,7 +1,38 @@
+import Image from "next/image"
+import logo from "@images/logo.png"
+
+
+
 const Footer = () => {
-    return (
-      <h1>Footer</h1>  
-    );
-  }
   
-  export default Footer;
+  return (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
+        <div className='flex flex-[0.5] justify-center items-center'>
+          <Image src={logo} alt="logo" className='w-32' />
+        </div>
+
+        <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
+          <p className='text-white text-base text-center mx-23 cursor-pointer'>Market</p>
+          <p className='text-white text-base text-center mx-23 cursor-pointer'>Exchange</p> 
+          <p className='text-white text-base text-center mx-23 cursor-pointer'>Tutorials</p> 
+          <p className='text-white text-base text-center mx-23 cursor-pointer'>Wallets</p>   
+        </div> 
+      </div>
+      <div className='flex justify-center items-center flex-col mt-5'>
+      <p className="text-white text-sm text-center">Come join us!</p>
+      <p className="text-white text-sm text-center font-medium mt-2">test@askdfhasd.com</p>
+      </div> 
+      <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+
+      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+        <a href="https://github.com/echomonk">
+          <p className="text-white text-left text-xs">github/echomonk</p>
+        </a>
+          <p className="text-white text-right text-xs">Learning project 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
