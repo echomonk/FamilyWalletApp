8f15457a9744a7d3adae7c261950ab8aecea1239
echomonk <erdaloylupinar@gmail.com>
2022-04-15 22:11:07 +0300

    Update index.js

--- components/common/walletpanel/index.js
+++ components/common/walletpanel/index.js
@@ -1,3 +1,3 @@
 export { default as addAllowance } from "./addAllowance"
-// export { default as reduceAllowance } from "./reduceAllowance"
-// export { default as withdraw } from "./withdraw"
+export { default as reduceAllowance } from "./reduceAllowance"
+export { default as withdraw } from "./withdraw"
