0f61e98f54336df3e6514b31597c41a403d811b4
echomonk <erdaloylupinar@gmail.com>
2022-04-15 12:06:33 +0300

    Update Transactions.js

--- components/ui/Transactions.js
+++ components/ui/Transactions.js
@@ -1,32 +1,30 @@
-import { SetForm } from "@components/context";
-import { useWeb3 } from "@components/provider";
-import { shortenAddress } from "@components/provider/web3/hooks/shortenAddress";
 import useFetch from "@components/provider/web3/hooks/useFetch";
-import { useAccount } from "@components/web3/hooks";
-import dummyData from "@utils/dummyData";
-import { useContext } from "react";
+import { shortenAddress } from "@components/provider/web3/hooks/shortenAddress";
+import { UseAccount, useTransaction } from "@components/web3/hooks";
+import { UseTxContext } from "@components/context/TransactionContext";
 
-const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+
+const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
   const gifUrl = useFetch({ keyword });
   return (
     <div className="bg-[#181918] m-4 flex flex-1
-    2xl:min-w-[450px]
-    2xl:max-w-[500px]
-    sm:min-w-[270px]
-    sm:max-w-[300px]
-    min-w-full
-    flex-col p-3 rounded-md hover:shadow-2xl"
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[300px]
+      min-w-full
+      flex-col p-3 rounded-md hover:shadow-2xl"
     >
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
-        <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
           </a>
           <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
           </a>
-          <p className="text-white text-base">Amount: {amount} ETH</p>
-          {message && (
+            <p className="text-white text-base">Amount: {amount} ETH</p>
+          { message && (
             <>
               <br />
               <p className="text-white text-base">Message: {message}</p>
@@ -47,28 +45,30 @@ const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, 
   )
 }
 
-
-
 const Transactions = () => {
-const { web3 } = useWeb3()
-const { account } = useAccount()
+const { transactions } = UseTxContext()
+const { account } = UseAccount()
+// const { transaction } = useTransaction()
+// console.log(transaction)
 
     return (
       <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
         <div className="flex flex-col md:p-12 py-12 px-4">
             { account.data ? (
               <h3 className="text-white text-3xl text-center my-2">
-                Latest Transactions
+              Latest Transactions
               </h3>
+              
               ) : (
               <h3 className="text-white text-3xl text-center my-2">
                 Connect your account to see the latest transactions
               </h3>
             )}
             <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => (
-            <TransactionsCard key={i} {...transaction} />
-          ))}
+              { transactions?.slice(-6).reverse().map((transaction, index) => (
+                <TransactionsCard key={index} {...transaction} />
+                ))
+              }
             </div>
         </div>
       </div>
