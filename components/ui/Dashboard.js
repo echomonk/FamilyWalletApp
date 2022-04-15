19137d77bfda643b3c93f0d3c8fac54f3df92249
echomonk <erdaloylupinar@gmail.com>
2022-04-15 17:54:41 +0300

    Update Dashboard.js

--- components/ui/Dashboard.js
+++ components/ui/Dashboard.js
@@ -1,49 +1,10 @@
-import { UseTxContext } from "@components/context/TransactionContext";
-import { UseWeb3 } from "@components/provider";
 import { UseAccount, useNetwork } from "@components/web3/hooks";
 import Tabs from "./tabs";
 
-// const Input = ({ placeholder, name, type, value, handleChange }) => (
-//   <input 
-//     placeholder={placeholder}
-//     type={type}
-//     min="0.0000"
-//     step="0.0001"
-//     value={value}
-//     onChange={(e) => handleChange(e, name)}
-//     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-//   />
-// )
-
-// const LINKS = [{
-//   href: "/walletpanel/addAllowance",
-//   value: "Add Allowance",
-//   requireAdmin: true
-// }, {
-//   href: "/walletpanel/reduceAllowance",
-//   value: "Reduce Allowance",
-//   requireAdmin: true
-// }, {
-//   href: "/walletpanel/withdraw",
-//   value: "Withdraw"
-// }]
-
 const Dashboard = ({}) => {
-  const { connect, isLoading, isWeb3Loaded, web3 } = UseWeb3()
-  const { handleChange1, formData, formData2, sendTx } = UseTxContext()
   const { account } = UseAccount()
   const { network } = useNetwork()
-  // const { tx } = useTransactions()
 
-  const handleSubmit = (e) => {
-    const { addressTo, amount } = formData2
-    
-    e.preventDefault()
-
-    if(!addressTo || !amount) return
-
-    sendTx(web3)
-  }
 
     return (
       <div className="flex flex-col w-full justify-center items-center my-5 p-3 blue-glassmorphism">
