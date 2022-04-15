f04f17da417b4fa22222b88c78d98a8692ebd2f6
echomonk <erdaloylupinar@gmail.com>
2022-04-15 12:06:21 +0300

    Update Footer.js

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -6,7 +6,8 @@ import logo from "@images/logo.png"
 const Footer = () => {
   
   return (
-    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+
+    <div className="w-full flex md:justify-center items-center flex-col p-4 gradient-bg-footer">
       <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
         <div className='flex flex-[0.5] justify-center items-center'>
           <Image src={logo} alt="logo" className='w-32' />
