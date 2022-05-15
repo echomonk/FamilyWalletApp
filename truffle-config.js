d404ef4965932d24ae9347b6dc10d051718d21b8
echomonk <erdaloylupinar@gmail.com>
2022-05-15 18:39:06 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,10 @@ module.exports = {
     development: {
      host: "127.0.0.1",
      port: 7545,
-     network_id: "*"
+     network_id: "*",
+    //  gasPrice: 20000000000,
+    //  gasLimit: 550000000
+
     },
   },
 
